3c51f43fa537bf14021575c92706abac891a2478
Twinkle <garg.twinkle08@gmail.com>
2024-03-12 21:19:56 +0530

    code review changes + added fields in table

---
+++ diffIT/force-app/main/default/lwc/lwcLookup/lwcLookup.js
@@ -0,0 +1,95 @@
+import { LightningElement, track, wire, api } from "lwc";
+import findRecords from "@salesforce/apex/ContactFormController.findRecords";
+export default class LwcLookup extends LightningElement {
+  @track recordsList;
+  @track searchKey = "";
+  @api selectedValue;
+  @api selectedRecordId;
+  @api objectApiName;
+  @api iconName;
+  @api disable;
+  @api lookupLabel;
+  @track message;
+  @api institutions;
+  @api coordinates;
+  lookupDebounceTimeoutId;
+
+  onLeave(event) {
+    setTimeout(() => {
+      this.searchKey = "";
+      this.recordsList = null;
+    }, 300);
+  }
+
+  @api
+  clearSelection() {
+    this.selectedRecordId = "";
+    this.selectedValue = "";
+    this.searchKey = "";
+    this.recordsList = null;
+  }
+
+  onRecordSelection(event) {
+    this.selectedRecordId = event.target.dataset.key;
+    this.selectedValue = event.target.dataset.name;
+    this.searchKey = "";
+    this.onSeletedRecordUpdate();
+  }
+
+  handleKeyChange(event) {
+    this.searchKey = event.target.value.trimStart();
+
+    // Clear the existing timeout on each key press
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+
+    // Set a new timeout
+    this.timeoutId = setTimeout(() => {
+      if (this.searchKey.length >= 3) {
+        this.getLookupResult();
+      }
+    }, 500);
+  }
+
+  removeRecordOnLookup(event) {
+    this.searchKey = "";
+    this.selectedValue = null;
+    this.selectedRecordId = null;
+    this.recordsList = null;
+    this.onSeletedRecordUpdate();
+  }
+
+  getLookupResult() {
+    findRecords({
+      searchKey: this.searchKey,
+      objectName: this.objectApiName,
+      institutions: this.institutions,
+      coordinates: this.coordinates
+    })
+      .then((result) => {
+        if (result.length === 0) {
+          this.recordsList = [];
+          this.message = "No Records Found";
+        } else {
+          this.recordsList = result;
+          this.message = "";
+        }
+        this.error = undefined;
+      })
+      .catch((error) => {
+        this.error = error;
+        this.recordsList = undefined;
+      });
+  }
+
+  onSeletedRecordUpdate() {
+    const passEventr = new CustomEvent("recordselection", {
+      detail: {
+        selectedRecordId: this.selectedRecordId,
+        selectedValue: this.selectedValue
+      }
+    });
+    this.dispatchEvent(passEventr);
+  }
+}
--- diffIT/force-app/main/default/lwc/schoolTable/schoolTable.js
+++ diffIT/force-app/main/default/lwc/schoolTable/schoolTable.js
@@ -2,11 +2,13 @@ import { LightningElement, api, track, wire } from "lwc";
 import findRecords from "@salesforce/apex/ContactFormController.findRecords";
 
 export default class SchoolTable extends LightningElement {
-  columns = [{ label: "Name", fieldName: "Name" }];
+  columns = [{ label: "Name", fieldName: "Name" },
+  { label: "City", fieldName: "BillingCity" },
+  { label: "State", fieldName: "BillingState" }];
   @api institutions;
   @api objectApiName;
   @api coordinates;
-  @track recordsList;
+  @track records;
   @track filteredRecords = [];
 
   get doesRecordExists(){
@@ -20,8 +22,8 @@ export default class SchoolTable extends LightningElement {
   })
   getSchoolData({error,data}){
     if(data){
-      this.recordsList = data;
-      this.filteredRecords = [...this.recordsList];
+      this.records = data;
+      this.filteredRecords = [...this.records];
     }
     if(error){
       console.error(error);
@@ -29,9 +31,9 @@ export default class SchoolTable extends LightningElement {
   }
   
   handleSchoolSearch(event) {
-    let searchString = event.target.value;
-    this.filteredRecords = this.recordsList.filter((ele) => {
-      return ele.Name?.toLowerCase().includes(searchString?.toLowerCase());
+    const searchString = event.target.value;
+    this.filteredRecords = this.records.filter((r) => {
+      return r.Name?.toLowerCase().includes(searchString?.toLowerCase());
     });
   }
   getSelectedAccount(event) {
